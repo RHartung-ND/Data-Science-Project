51db5f4f2884458f497bb7d615775cf8b3a2501b
Matthew Lad <37879186+matt0681@users.noreply.github.com>
2025-02-13 14:23:04 -0500

    creating a jupyter notebook for playing with the data

--- looking-at-data.py
+++ looking-at-data.py
@@ -9,7 +9,12 @@ num_rows, num_columns = df.shape
 st.write(f"Number of Columns: {num_columns}")
 st.write(f"Number of Rows: {num_rows}")
 
+unique_column_values = df.nunique(0)
+st.write(f"How many values in each column are unique: {unique_column_values}")
+
 
 # Showing simple statistics the numerical column on the dataset
 st.write("**Summary Statistics for Numerical Attributes**")
 st.dataframe(df.describe())
+
+
